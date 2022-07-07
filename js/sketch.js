29fa7e57cd03b22e91275d419dee6208e6ea07c0
koya-kimura <2ktmnri8@gmail.com>
2022-07-07 12:04:43 +0900

    first commit

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,11 @@
 let sketch; //出力するファイルの名前を定義
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight, WEBGL);
+    resizeCanvas(windowWidth, 650, WEBGL);
 }
 
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    canvas = createCanvas(windowWidth, 650, WEBGL);
     canvas.position(0, 0); //canvasをページの原点に固定
     canvas.style('z-index', '-1'); //canvasを後ろに移動する
 }
@@ -19,7 +19,7 @@ function draw() {
         push();
         rotateX((frameCount + i*10) / 100);
         rotateY((frameCount + i*10) / 100);
-        box(600);
+        box(450+sin(frameCount/200)*300);
         pop();
     }
 }
